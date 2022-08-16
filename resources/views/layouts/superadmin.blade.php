161a00917af5d628649392a99fa31b84f095a62f
Syahrizal Alisadikin <56682176+syahrizal-alisadikin@users.noreply.github.com>
2022-08-16 22:11:54 +0700

    up

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -69,69 +69,71 @@
         <!-- /#sidebar-wrapper -->
         {{-- Navbar --}}
          <div id="page-content-wrapper">
-          <nav
-            class="navbar navbar-store navbar-expand-lg navbar-light fixed-top"
+       <nav
+            class="navbar navbar-expand-lg navbar-light navbar-store fixed-top"
             data-aos="fade-down"
           >
-            <button
-              class="btn btn-secondary d-md-none mr-auto mr-2"
-              id="menu-toggle"
-            >
-              &laquo; Menu
-            </button>
-
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto d-none d-lg-flex">
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    <img
-                      src="/images/icon-user.png"
-                      alt=""
-                      class="rounded-circle mr-2 profile-picture"
-                    />
-                    Hi, {{Auth::user()->name}}
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div class="container-fluid">
+              <button
+                class="btn btn-secondary d-md-none mr-auto mr-2"
+                id="menu-toggle"
+              >
+                &laquo; Menu
+              </button>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Dekstop Menu-->
+                <ul class="navbar-nav d-none d-lg-flex ml-auto">
+                  <li class="nav-item dropdown">
+                    <a
+                      href="#"
+                      class="nav-link"
+                      id="navbarDropdown"
+                      role="button"
+                      data-toggle="dropdown"
+                    >
+                      <img
+                        src="/images/icon-user.png"
+                        alt=""
+                        class="rounded-circle mr-2 profile-picture"
+                      />Hi, Admin</a
+                    >
+                    <div class="dropdown-menu">
+                      
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                                                  document.getElementById('logout-form').submit();">
+                         {{ __('Logout') }}
+                 </a>
+                            
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                  </form>
+                    </div>
+                  </li>
                   
-                    {{-- <div class="dropdown-divider"></div> --}}
-                    <a class="dropdown-item" href="/">Logout</a>
-                  </div>
-                </li>
-               
-              </ul>
-              <!-- Mobile Menu -->
-              <ul class="navbar-nav d-block d-lg-none mt-3">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Hi,  {{Auth::user()->name}}
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-inline-block" href="#">
-                    Cart
-                  </a>
-                </li>
-              </ul>
+                </ul>
+
+                <ul class="navbar-nav d-block d-lg-none">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link"> Hi, Admin </a>
+                  </li>
+                </ul>
+                <ul class="navbar-nav d-block d-lg-none">
+                  <li class="nav-item">
+                    <a href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();" class="nav-link">Logout</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </nav>
           <!-- Page Content -->
