f680de5f502d18328c20ccad1b0f62d7d4afdff5
Syahrizal Alisadikin <56682176+syahrizal-alisadikin@users.noreply.github.com>
2022-08-16 22:13:44 +0700

    up

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -144,25 +144,18 @@
     </div>
     <!-- Bootstrap core JavaScript -->
     @stack('prepend-style')
-   <script
-  src="https://code.jquery.com/jquery-3.6.0.js"
-  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-  crossorigin="anonymous"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.22/datatables.min.js"></script>
-
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
       AOS.init();
     </script>
-    <!-- Menu Toggle Script -->
     <script>
-    
       $("#menu-toggle").click(function (e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
       });
-    
     </script>
     @stack('addon-script')
   </body>
