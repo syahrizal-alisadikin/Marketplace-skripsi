777f0e952f95d684b34736a39044b9e392908233
Syahrizal Alisadikin <56682176+syahrizal-alisadikin@users.noreply.github.com>
2022-08-16 22:06:55 +0700

    up

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -142,8 +142,10 @@
     </div>
     <!-- Bootstrap core JavaScript -->
     @stack('prepend-style')
-    {{-- <script src="/vendor/jquery/jquery.min.js"></script> --}}
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+   <script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.22/datatables.min.js"></script>
 
